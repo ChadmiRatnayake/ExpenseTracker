d4f4b012fab8b88a750dc5a0b2195dd0f794af12
ChadmiRatnayake <chadmi.20@cse.mrt.ac.lk>
2023-12-12 17:04:38 +0530

    frontend implementations

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ListExpenseComponent } from './components/list-expense/list-expense.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule,  RouterOutlet, ListExpenseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ FrontEnd/src/app/components/list-expense/list-expense.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-expense',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-expense.component.html',
+  styleUrl: './list-expense.component.css'
+})
+export class ListExpenseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+      
+  }
+
+}
